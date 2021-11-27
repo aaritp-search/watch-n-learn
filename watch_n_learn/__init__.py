e8808992cbf823d9bd945f83e9c14e0dc869f517
Brian Law <gmbrianlaw@gmail.com>
2021-11-27 00:44:37 -0800

    Create ping and 404 routes

--- watch_n_learn/__init__.py
+++ watch_n_learn/__init__.py
@@ -2,8 +2,17 @@ __all__ = ["create_server"]
 
 from fastapi.applications import FastAPI
 
+from watch_n_learn.router.error import error_router
+
 def create_server(debug__: bool) -> FastAPI:
 
     server = FastAPI(debug=debug__)
+
+    @server.head("/ping")
+    def ping() -> None:
+
+        return None
+
+    server.include_router(error_router)
 
     return server
---
+++ watch_n_learn/helper/constant.py
@@ -0,0 +1,3 @@
+from fastapi.templating import Jinja2Templates
+
+TEMPLATE = Jinja2Templates("watch_n_learn/template")
